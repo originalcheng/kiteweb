dc9a8acf53357b11ed130fee6af8731484867798
originalcheng <chengh@gmail.com>
2019-11-15 07:52:56 -0800

    add home page

--- helloworld/views.py
+++ helloworld/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, Clint and Ramsey and Baha and Deniz.")
+    return HttpResponse("Hello, Saul.")
--- kiteweb/urls.py
+++ kiteweb/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('helloworld/', include('helloworld.urls')),
+    path('', include('helloworld.urls')),
 ]
