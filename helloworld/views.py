76e99354892beb5caad6bec1fc0bc2e9484d010d
originalcheng <chengh@gmail.com>
2020-03-03 13:31:56 -0800

    test context processory

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
-
+# from django.http import HttpResponse
+from django.shortcuts import render
+# from django.views.generic import TemplateView, View
 
 def index(request):
-    return HttpResponse("Hello, Saul.")
+    # return HttpResponse("Hello, Saul.")
+    return render(request, "index.html", {})
